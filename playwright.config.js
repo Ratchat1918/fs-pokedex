1d316cba6b4761fe64cd451602951d5b6218c25b
ratchat1918 <eusenko101@gmail.com>
2026-07-02 18:57:10 +0300

    exercise 9 try 7

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,6 @@ export default defineConfig({
     command: 'npm run start',
     url: 'http://localhost:8080',
     timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,//eslint-disable-line no-unused-vars no-undef
   },
   use: {
     baseURL: 'http://localhost:8080/',
