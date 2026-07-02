c34972131f3fae62b77721664751131378baa4d9
ratchat1918 <eusenko101@gmail.com>
2026-07-02 18:50:57 +0300

    exercise 9 try 4(second playwright test)

--- playwright.config.js
+++ playwright.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   webServer: {
     command: 'npm run start',
-    url: 'http://localhost:3000',
-    timeout: 120 * 1000
+    url: 'http://localhost:8080',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,//eslint-disable-line no-unsed-vars
   },
   use: {
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'http://localhost:8080/',
   },
+  testIgnore: ['**/test/**'],
 });
