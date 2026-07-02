6a00113d8ed842377a2257acb3e6c48f79c0f03f
ratchat1918 <eusenko101@gmail.com>
2026-07-02 18:12:45 +0300

    exercise 9 try 2

--- playwright.config.js
+++ playwright.config.js
@@ -3,8 +3,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run start',
     url: 'http://localhost:3000',
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000
   },
   use: {
     baseURL: 'http://localhost:3000/',
